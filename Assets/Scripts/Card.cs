8f4aca8f51d2276df3b9b96af908e366215e174a
EthanBarlevy <98473724+EthanBarlevy@users.noreply.github.com>
2024-02-22 11:13:08 -0700

    card prefabs

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -7,13 +7,35 @@ public class Card : MonoBehaviour
 {
     [SerializeField] int value;
     [SerializeField] Suit suit;
-    [SerializeField] Sprite image;
+    [SerializeField] Sprite cardFront;
+    [SerializeField] Sprite cardBack;
+    [SerializeField] bool flipped;
     SpriteRenderer spriteRenderer;
 
 	private void Start()
 	{
 		spriteRenderer = GetComponent<SpriteRenderer>();
-        spriteRenderer.sprite = image;
+        if (flipped)
+        {
+            spriteRenderer.sprite = cardFront;
+        }
+        else 
+        {
+            spriteRenderer.sprite = cardBack;
+        }
+	}
+
+    public void Flip() 
+    {
+        flipped = !flipped;
+		if (flipped)
+		{
+			spriteRenderer.sprite = cardFront;
+		}
+		else
+		{
+			spriteRenderer.sprite = cardBack;
+		}
 	}
 }
 
